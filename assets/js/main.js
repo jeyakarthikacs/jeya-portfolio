ba5909218927692409583f9c633e5b96ebd74869
jeyakarthikacs <jeyakarthikacs@gmail.com>
2025-04-02 12:27:18 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+/*========= MENU =======*/
+
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle');
+
+/* Menu Show - hidden*/
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show-menu');
+    navToggle.classList.toggle('animate-toggle');
+});
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SERVICES SWIPER ===============*/
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+/* Active work */
+
+/*=============== RESUME ===============*/
+
+/*=============== TESTIMONIALS SWIPER ===============*/
+
+/*=============== EMAIL JS ===============*/
+
+/*=============== STYLE SWITCHER ===============*/
+
+/* Switcher show */
+
+/* Switcher hidden */
+
+/*=============== THEME COLORS ===============*/
+
+/*=============== LIGHT/DARK MODE ===============*/
