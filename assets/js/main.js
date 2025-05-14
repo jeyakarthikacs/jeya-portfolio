aaf3fdd1e2343f751455187070aefe5ee575467e
jeyakarthikacs <jeyakarthikacs@gmail.com>
2025-05-14 23:46:16 +0530

    3.44.56

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,28 @@ function activeWork() {
 }
 linkWork.forEach((a) => a.addEventListener('click', activeWork));
 /*=============== RESUME ===============*/
+const accordionItems = document.querySelectorAll('.resume-item');
+accordionItems.forEach((item) => {
+    const header = item.querySelector('.resume-header'),
+    content = item.querySelector('.resume-content'),
+    icon = item.querySelector('.resume-icon i');
+
+    header.addEventListener('click', () => {
+        const isOpen = item.classList.toggle('accordion-open');
+
+        content.style.height = isOpen ? content.scrollHeight + 'px' : '0';
+        icon.className = isOpen ? 'ri-subtract-line' : 'ri-add-line';
+
+        accordionItems.forEach((otherItem) => {
+            if(otherItem !== item && otherItem.classList.contains('accordion-open')) {
+                otherItem.classList.remove('accordion-open');
+                otherItem.querySelector('.resume-content').style.height = '0';
+                otherItem.querySelector('.resume-icon').classList = 'ri-add-line';
+            }
+        })
+    });
+
+});
 
 /*=============== TESTIMONIALS SWIPER ===============*/
 
