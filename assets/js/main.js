73147ca88b65818586a5313a6ba737568ca90556
jeyakarthikacs <jeyakarthikacs@gmail.com>
2025-05-18 23:15:06 +0530

    4.58.22

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,10 +10,49 @@ navToggle.addEventListener('click', () => {
 });
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink =document.querySelectorAll('.nav-link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+
+    navToggle.classList.remove('animate-toggle');
+    navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach((n) => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 20 
+        ? header.classList.add('bg-header') 
+        : header.classList.remove('bg-header');
+};
+
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop,
+        sectionId = current.getAttribute('id'),
+        sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link');
+        } else {
+            sectionsClass.classList.remove('ative-link');
+        }
+    });
+};
+
+window.addEventListener('load', scrollActivate);
 
 /*=============== SERVICES SWIPER ===============*/
 var servicesSwiper = new Swiper('.services-swiper',{
